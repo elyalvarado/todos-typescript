4ecd268c86c1c1162df67c0e939a9e2d5e2910f3
elyalvarado <elyalvarado@gmail.com>
2019-07-07 20:23:35 -0500

    Migrate the ducks and their specs to typescript

--- src/ducks/visibilityFilter.ts
+++ src/ducks/visibilityFilter.ts
@@ -9,23 +9,28 @@
 // 🚀 Use the Reducer type from redux to type the reducer function (hint:
 //    it uses the type with state shape
 
-export const VisibilityFilters = {
-  SHOW_ALL: "SHOW_ALL",
-  SHOW_COMPLETED: "SHOW_COMPLETED",
-  SHOW_ACTIVE: "SHOW_ACTIVE"
-};
+import { Reducer } from "redux";
+
+export enum VisibilityFilters {
+  SHOW_ALL = "SHOW_ALL",
+  SHOW_COMPLETED = "SHOW_COMPLETED",
+  SHOW_ACTIVE = "SHOW_ACTIVE"
+}
 
 // Actions
 const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
 
 // Action Creators
-export const setVisibilityFilter = filter => ({
+export const setVisibilityFilter = (filter: VisibilityFilters) => ({
   type: SET_VISIBILITY_FILTER,
   filter
 });
 
 // Reducer
-const reducer = (state = VisibilityFilters.SHOW_ALL, action) => {
+const reducer: Reducer<VisibilityFilters> = (
+  state = VisibilityFilters.SHOW_ALL,
+  action
+) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return action.filter;
